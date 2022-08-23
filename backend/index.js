3ff17fc6add85efaa700da241e9908c6013af6ea
Nemo3003 <ulimarian2000@gmail.com>
2022-08-23 11:35:56 -0300

    [UPD]Beginning API

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
+//Execute: ./node_modules/.bin/nodemon index.js
+const notes = [
+    {
+        "title": "nota 1",
+        "content": "nota 1 contentwerwewr",
+        },
+        {
+        "title": "nota 2",
+        "content": "nota 2 content",
+        },
+        {
+        "title": "nota 3",
+        "content": "nota 3 content",
+
+    }
+]
 const app = http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World');
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(notes));
 })
 
 const PORT = 3001
