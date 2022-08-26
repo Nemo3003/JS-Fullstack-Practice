1d65f2c67b2023801b70f8c1894dd2c9c7b1cba4
Nemo3003 <ulimarian2000@gmail.com>
2022-08-26 11:24:37 -0300

    [UPD]Express Server

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const { application, response } = require('express');
+const express = require('express');
+const app = express();
 
 //Execute: ./node_modules/.bin/nodemon index.js
 const notes = [
@@ -16,11 +18,27 @@ const notes = [
 
     }
 ]
-const app = http.createServer((req, res)=>{
+/*const app = http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end(JSON.stringify(notes));
 })
-
+*/
+//Esto nos permitira ir a ciertas direcciones para obtener un determinado dato
+app.get('/', (req, res)=>{
+    res.send('<h1>Welcome!</h1>');
+})
+app.get('/api/notes', (req, res)=>{
+    res.json(notes);
+})
+//el ':id' es una manera dinamica de recuperar un segmento del path
+app.get('/api/notes/:id', (req, res)=>{
+    const id = +req.params.id
+    const note = notes.find(note => note.id === id);
+    res.json(note);
+})
+//Express es asincrono por lo que esta es la manera correcta de ponerle.
+//La arquitectura REST nos permite construir aplicaciones escalables
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+} )
