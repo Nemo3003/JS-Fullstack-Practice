ec9080d6314a54cfb434ec883452016862180515
Nemo3003 <ulimarian2000@gmail.com>
2022-10-16 17:02:52 -0300

    [UPD] More on graphql

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,4 +1,4 @@
-import {gql} from "apollo-server"
+import {ApolloServer, gql} from "apollo-server"
 
 const persons = [
     {
@@ -6,6 +6,7 @@ const persons = [
         phone: "123",
         street: "av fakestreet",
         city: "Mountain",
+        check: "John",
         id: "122jh3j2b"
     },
     {
@@ -13,6 +14,7 @@ const persons = [
         phone: "1233",
         street: "av fakestreet",
         city: "Mountain",
+        check: "Juan",
         id: "122jh3j2b"
     },
     {
@@ -20,20 +22,54 @@ const persons = [
         phone: "1233",
         street: "av fakestreet",
         city: "Mountain",
+        check: "Hans",
         id: "122jh3j2b"
     }
 ]
-const typeDefs = gql`
+const typeDefinitions = gql`
+    type Address {
+        street: String!
+        city: String!
+    }
+
     type Person{
         name: String!
         phone: String
-        street: String!
-        city: String!
+        address: Address!
         id: ID!
 }
     type Query{
         personCount: Int!
         allPersons: [Person]!
+        findPerson(name: String): Person
     }
 `
+
+const resolvers = {
+    Query: {
+        personCount: ()=> persons.length,
+        allPersons: ()=> persons,
+        findPerson: (root,args)=>{
+            const{name} = args
+            return persons.find(person => person.name === name)
+        }
+    },
+    Person: {
+        address: (root) =>{
+            return {
+                street: root.street,
+                city: root.city
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: typeDefinitions,
+    resolvers
+})
+
+server.listen().then(({url})=>{
+    console.log('listening on '+url);
+})
 //DEFINICION DE DATOS Y DE DONDE SACAMOS LOS DATOS. ESENCIAL EN GRAPHQL
